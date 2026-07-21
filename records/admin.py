4a427f3d5f65c027f652f0c61d7649a9b38ab22b
Alisher Voitov <voitovalisher020@gmail.com>
2026-07-21 10:35:44 +0300

    admin panel

--- records/admin.py
+++ records/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import AuditLog, MedicalRecord, Patient
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "gender", "primary_doctor"]
+    search_fields = ["user__username", "user__first_name", "user__last_name"]
+
+
+@admin.register(MedicalRecord)
+class MedicalRecordAdmin(admin.ModelAdmin):
+    list_display = ["id", "patient", "record_type", "title", "created_by", "visit_date"]
+    list_filter = ["record_type"]
+
+
+@admin.register(AuditLog)
+class AuditLogAdmin(admin.ModelAdmin):
+    list_display = ["id", "timestamp", "actor", "action", "patient", "ip_address"]
+    list_filter = ["action"]
+    readonly_fields = [f.name for f in AuditLog._meta.fields]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False

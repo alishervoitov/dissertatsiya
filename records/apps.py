f8ee79781c728152e5678626ba92714bd9c6a826
Alisher Voitov <voitovalisher020@gmail.com>
2026-07-27 10:09:54 +0300

    records

--- records/apps.py
+++ records/apps.py
@@ -3,3 +3,12 @@ from django.apps import AppConfig
 
 class RecordsConfig(AppConfig):
     name = 'records'
+
+from django.apps import AppConfig
+
+class RecordsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'records'
+
+    def ready(self):
+        from . import signals  # noqa: F401

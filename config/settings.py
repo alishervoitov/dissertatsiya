beb20bc68cb4f281c2cbd83285dd8899c27882ae
Alisher Voitov <voitovalisher020@gmail.com>
2026-08-04 15:51:57 +0300

    something is fixed in settings.py

--- config/settings.py
+++ config/settings.py
@@ -146,6 +146,6 @@ AUTH_USER_MODEL = 'users.User'
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
